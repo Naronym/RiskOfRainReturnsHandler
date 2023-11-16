3435cae30ec24050dce6b19648c4409963afd573
Naronym <lucas.garcia34350@gmail.com>
2023-11-16 16:22:59 +0100

    Initial Release

---
+++ Risk of Rain Returns.js
@@ -0,0 +1,37 @@
+Game.FileSymlinkExclusions = ["steam_appid.txt", "xinput1_4.dll"];
+Game.FileSymlinkCopyInstead = ["options.ini"];
+Game.UseGoldberg = true;
+Game.UseNucleusEnvironment = true;
+Game.GoldbergExperimental = true; 
+Game.GoldbergNoLocalSave = true;
+Game.HandlerInterval = 100;
+Game.SymlinkExe = false;
+Game.SymlinkGame = true;
+Game.SymlinkFolders = true;
+Game.KeepSymLinkOnExit = true;
+Game.GameName = "Risk of Rain Returns";
+Game.SteamID = "1337520";
+Game.GUID = "Risk of Rain Returns";
+Game.ExecutableName = "Risk of Rain Returns.exe";
+Game.MaxPlayersOneMonitor = 4;
+Game.MaxPlayers = 4;
+Game.HideTaskbar = false;
+Game.RefreshWindowAfterStart = true;
+Game.ResetWindows = true;
+Game.Hook.DInputEnabled = false;
+Game.Hook.XInputEnabled = true;
+Game.Hook.XInputReroute = false;
+Game.XInputPlusDll = ["xinput1_4.dll"];
+Game.Hook.CustomDllEnabled = false;
+Game.SupportsKeyboard = true;
+Game.FakeFocus = true;
+Game.FakeFocusInterval = 8000;
+Game.SendFakeFocusMsg = true;
+Game.HookFocus = true;
+Game.Hook.ForceFocus = true;
+Game.Description =
+"Allow splitscreen experience for Risk of Rain Returns. If you want to keep your save you need to locate your RoRR savefile first and copy it for each player in C:/Users/<Username>/NucleusCoop/PlayerX/Appdata/Roaming/Goldberg SteamEmu Saves/1337520/remote. Make sure to disable Steam Input for the game and hide your DS4 or DualSense controller";
+Game.PromptBetweenInstances = true;
+Game.PauseBetweenStarts = 20;
+
+
